54cf190f7bf63f064efd0634af77724c9269137f
Dennis7456 <denniskiplangat.dk@gmail.com>
2023-01-11 02:48:38 +0300

    initial setup

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+   
+</x-app-layout>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<h1>Admin Dashboard</h1>
+</body>
+</html>
